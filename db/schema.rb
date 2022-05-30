e6a6f89eb832023430cf3397a761e4eb9b3ded67
Kevin Nguyen <kevintngu@hotmail.com>
2022-05-30 17:36:26 +1000

    initialising pages and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,16 @@ ActiveRecord::Schema.define(version: 2022_05_30_062847) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "username", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "puppies"
