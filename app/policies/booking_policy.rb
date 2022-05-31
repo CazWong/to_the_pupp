438117eb907fc14b9e2c1068a5f4b51fde1d1d3e
Kevin Nguyen <kevintngu@hotmail.com>
2022-05-31 15:38:36 +1000

    added policy files for booking and puppy

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,26 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    authorised_user?
+  end
+
+  def destroy?
+    authorised_user?
+  end
+
+  private
+
+  def authorised_user?
+    record.user == user
+  end
+end
---
+++ app/policies/puppy_policy.rb
@@ -0,0 +1,35 @@
+class PuppyPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+  # TODO
+
+  def index
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    authorised_user?
+  end
+
+  def destroy?
+    authorised_user?
+  end
+
+  private
+
+  def authorised_user?
+    record.user == user
+  end
+end
