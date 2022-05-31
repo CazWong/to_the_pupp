980f5ec8ea2875091f672259e32314b6c8b098e5
Kevin Nguyen <kevintngu@hotmail.com>
2022-05-31 12:31:49 +1000

    seeded users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(username: Faker::Internet.user[:username], email: Faker::Internet.user[:email], encrypted_password: 'secret')
+end
